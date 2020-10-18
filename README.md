ee4ebfed06076c68e1b0b316b75803507a1bd80b
DanBrown47 <danwnad47@gmail.com>
2020-10-18 14:34:05 +0530

    Multiple commits => Main commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# E N I G M A
+>  To convey data out of a system not via any port (Virtual/Physical) nor network but through keyboard lighting morse code.
+>  A data retrival tool which ofcourse may be the last option.
+
+## Installation
+- Should first clone the entire repository of enigma
+- Installing the dependancy
+
+
+### Clone
+
+- Clone this repo to your local machine using `https://github.com/DanBrown47/enigma.git'
+- Or download and extract the zip
+
+### Setup
+ 
+ - Install dependancy of virtkey 
+ ```shell
+$ sudo apt-get install python3-virtkey
+```
+
+## Contributing
+
+> To get started...
+
+### Step 1
+
+- **Option 1**
+    - 🍴 Fork this repo!
+
+- **Option 2**
+    - 👯 Clone this repo to your local machine using `https://github.com/DanBrown47/enigma.git`
+
+### Step 2
+
+- **HACK AWAY!** 🔨🔨🔨
+  - Append remote modules.
+  - Make it universal (MAC / Windows)
+
+### Step 3
+
+- 🔃 Create a new pull request using <a href="https://github.com/DanBrown47/enigma/compare/" target="_blank">`https://github.com/DanBrown47/enigma/compare/`</a>.
+
+---
+
+## License
+
+[![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://badges.mit-license.org)
+
+- **[MIT license](http://opensource.org/licenses/mit-license.php)**
+- Copyright 2020 © Danwand
+
---
+++ enigma.py
@@ -0,0 +1,77 @@
+import virtkey
+import time
+import os
+# more
+# more
+
+
+v = virtkey.virtkey()
+
+def capson():
+
+    time.sleep(0.1)
+    v.press_keycode(66)    # turns capslock on
+    v.release_keycode(66)
+
+def capsoff():
+    time.sleep(0.1)
+    v.press_keycode(66)    # turns capslock off
+    v.release_keycode(66)
+
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-',
+                    'a':'.-', 'b':'-...',
+                    'c':'-.-.', 'd':'-..', 'e':'.',
+                    'f':'..-.', 'g':'--.', 'h':'....',
+                    'i':'..', 'j':'.---', 'k':'-.-',
+                    'l':'.-..', 'm':'--', 'n':'-.',
+                    'o':'---', 'p':'.--.', 'q':'--.-',
+                    'r':'.-.', 's':'...', 't':'-',
+                    'u':'..-', 'v':'...-', 'w':'.--',
+                    'x':'-..-', 'y':'-.--', 'z':'--..',
+                    }
+
+
+
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+
+            # Looks up the dictionary and adds the
+            # correspponding morse code
+            # along with a space to separate
+            # morse codes for different characters
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+            # 1 space indicates different characters
+            # and 2 indicates different words
+            cipher += ' '
+
+    print(cipher)
+    sleev = list(cipher)
+
+    for i in sleev:
+        if i == ".":
+            capson()
+        elif i == "-":
+            capsoff()
+
+
+message = input("Enter your secret code for enigma\n\n")
+
+encrypt(message)
